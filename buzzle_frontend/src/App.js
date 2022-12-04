08062122a6d87a25d244582a91530beb38731539
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-12-04 01:49:11 +0100

    grid en construction

--- buzzle_frontend/src/App.js
+++ buzzle_frontend/src/App.js
@@ -1,9 +1,83 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [base, setBase] = useState("loading");
+  const [player, setPlayer] = useState("loading");
+  const [grid, setGrid] = useState(base);
+
+  //--baseBuilder
+  useEffect(() => {
+    //-- level est provisoire
+    const level = {
+      base: [
+        "WWWWWWWWWWWWWWWWWWWW",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "W..................W",
+        "WWWWWWWWWWWWWWWWWWWW",
+      ],
+      Spawn: [5, 2],
+    };
+    //-- baseBuilder construit le tableau du niveau choisi
+    const baseBuilder = (lvl) => {
+      let base = [];
+      for (let L = 0; L < lvl.length; L++) {
+        let lign = [];
+        for (let o = 0; o < lvl[0].length; o++) {
+          lign.push(lvl[L][o]);
+        }
+        base.push(lign);
+      }
+      return base;
+    };
+    //-- ici, je
+    setBase(baseBuilder(level.base));
+  }, []);
+
+  //-- gridDrawer
+  useEffect(() => {
+    let newGrid = [];
+    for (let L = 0; L < base.length; L++) {
+      let newLign = [];
+      for (let o = 0; o < base[0].length; o++) {
+        newLign.push("y");
+      }
+      newGrid.push(newLign);
+    }
+    setGrid(newGrid);
+  }, [base]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <section>
+        {grid !== "loading" ? (
+          <div className="table">
+            {grid.map((L, indexL) => {
+              return (
+                <div className="ligns" key={indexL}>
+                  {L.map((o, indexo) => {
+                    return (
+                      <div className="pos" key={indexo}>
+                        {o}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>yo</div>
+        )}
+      </section>
     </div>
   );
 }
