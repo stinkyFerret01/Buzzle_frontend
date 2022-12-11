5c5676d443224f2f7348f3011c5a9065e3685d54
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-12-11 19:53:44 +0100

    video link

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -8,13 +8,13 @@ const HomePage = () => {
   return (
     <main className="homePage">
       <div className="presVidsDisplay">
-        <div className="presVidDisplay">
+        <div className="presVidDisplay" onClick={() => navigate("/game/game")}>
           <h3>PLAY!</h3>
           <video width="220" height="130" loop autoPlay muted>
             <source src="../Videos/presVid.mp4" type="video/mp4" />
           </video>
         </div>
-        <div className="presVidDisplay">
+        <div className="presVidDisplay" onClick={() => navigate("/editor")}>
           <h3>EDIT!</h3>
           <video width="220" height="130" loop autoPlay muted>
             <source src="../Videos/presVidEdit.mp4" type="video/mp4" />
