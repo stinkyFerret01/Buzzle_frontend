8dd1aa056d3d7f72746e2fe46f2d68c0f73c73a0
Lafon Christophe <lafonchristophe5@gmail.com>
2022-12-20 14:04:15 +0100

    safe save

--- src/components/areYouSure.js
+++ src/components/areYouSure.js
@@ -37,7 +37,7 @@ const AreYouSure = ({
   return (
     <section className="spreadOver">
       <article className="ays">
-        <h3>ATTENTION!</h3>
+        <h3 className="noHovText">ATTENTION!</h3>
         <h4 className="noHovText">
           SI VOUS JOUEZ, VOUS RISQUEZ DE PERDRE VOTRE PROGRESSION!
         </h4>
